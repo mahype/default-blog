3841de600accf47cf6dd33517774cc7437495fae
Sven Wagener <sven@awesome.ug>
2017-02-01 15:31:18 +0100

    Formatted code, deleted test values and moved registration of settings to all admin sections

--- admin.php
+++ admin.php
@@ -8,22 +8,24 @@ $elements = array();
 
 tk_form_start( 'default-blog-config' );
 
-if( is_array( $plugins ) ):
-	foreach( $plugins AS $plugin ):
-			
+if ( is_array( $plugins ) ):
+	foreach ( $plugins AS $plugin ):
+
 		// If Templates are added and Template ID is set OR tab is blog template settings
-		if( ( DFB_TEMPLATE_EDIT_ID > 0  || 'blog-template' == $plugin['slug'] ) && function_exists( $plugin[ 'function_admin' ] ) ):
-		
-			$element[ 'id' ] = $plugin['slug'];
-			$element[ 'title' ] = $plugin['title'];
+		if ( ( DFB_TEMPLATE_EDIT_ID > 0 || 'blog-template' == $plugin['slug'] ) && function_exists( $plugin['function_admin'] ) ):
+
+			$element['id']    = $plugin['slug'];
+			$element['title'] = $plugin['title'];
 			
 			ob_start();
-			if( function_exists( $plugin[ 'function_admin' ] ) ) call_user_func( $plugin[ 'function_admin' ] );
-			$element[ 'content' ] = ob_get_clean();
-			
+			if ( function_exists( $plugin['function_admin'] ) ) {
+				call_user_func( $plugin['function_admin'] );
+			}
+			$element['content'] = ob_get_clean();
+
 			$elements[] = $element;
 		endif;
-			
+
 	endforeach;
 endif;
 
@@ -31,12 +33,12 @@ echo tk_tabs( 'default_blog_tabs', $elements, 'html' );
 
 echo tk_form_button( __( 'Save Settings', 'default-blog-options' ) );
 
-$content = tk_form_end( FALSE );
+$content = tk_form_end( false );
 
 ?>
 <div class="wrap">
     <h2><?php _e( 'Default Blog', 'default-blog-options' ); ?></h2>
     <p><?php _e( 'Create your Blog Template and setup which things you want to copy to new created blogs.', 'default-blog-options' ); ?></p>
-    <?php echo $content; ?>
-    
+	<?php echo $content; ?>
+
 </div>
